3447282d9baa55b7b2a3cd029e191d90ce8c3d87
Sergio Segrera <sergioarturosegrera@gmail.com>
2023-02-25 19:59:25 -0500

    Add color palette to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,33 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: '#FCF7F6',
+        black: '#282524',
+        orange: {
+          DEFAULT: '#FEBE73',
+        },
+        pink: {
+          DEFAULT: '#F2AABD',
+        },
+        green: {
+          DEFAULT: '#B5F2A4',
+        },
+        yellow: {
+          DEFAULT: '#FEDF74',
+        },
+        red: {
+          DEFAULT: '#F3777D',
+        },
+        purple: {
+          DEFAULT: '#B09AED',
+        },
+        blue: {
+          DEFAULT: '#A0D1DC',
+        },
+      },
+    },
   },
   plugins: [],
 };
