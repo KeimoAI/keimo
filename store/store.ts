2fa98bad94a576cab30a1f791b7388511c7f36ee
Sergio Segrera <sergioarturosegrera@gmail.com>
2023-02-26 17:05:17 -0500

    Add minimal state machine

--- store/store.ts
+++ store/store.ts
@@ -1,20 +1,44 @@
-import create from 'zustand';
+import { create } from 'zustand';
 
-export enum States {
+export enum State {
   IDLE,
   LISTENING,
   THINKING,
   SPEAKING,
 }
 
+type StateT = State.IDLE | State.LISTENING | State.THINKING | State.SPEAKING;
+
 type Store = {
-  state: States;
-  changeState: (state: States) => void;
+  state: StateT;
+  changeState: (state: StateT) => void;
+  updateState: () => void;
 };
 
 const useStore = create<Store>()((set) => ({
-  state: States.IDLE,
-  changeState: (state: States) => set({ state }),
+  state: State.IDLE,
+  changeState: (state: StateT) => set({ state }),
+  updateState: () =>
+    set((state) => {
+      let newState: StateT = state.state;
+
+      switch (state.state) {
+        case State.IDLE:
+          newState = State.LISTENING;
+          break;
+        case State.LISTENING:
+          newState = State.THINKING;
+          break;
+        case State.THINKING:
+          newState = State.SPEAKING;
+          break;
+        case State.SPEAKING:
+          newState = State.IDLE;
+          break;
+      }
+
+      return { state: newState };
+    }),
 }));
 
 export default useStore;
