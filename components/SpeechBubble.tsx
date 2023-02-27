ca7600eebd59ff3d1e9bb360ee67508f7108f537
Javier A. Pollak <javi.po.123@gmail.com>
2023-02-27 13:15:24 -0500

    Impl SpeechBubble component

---
+++ components/SpeechBubble.tsx
@@ -0,0 +1,84 @@
+'use client';
+
+import Ellipsis from './Ellipsis';
+
+const OverflowIndicator = ({ className }: { className?: string }) => (
+  <div className={`flex flex-col ${className}`}>
+    <div className="flex-initial flex flex-col items-center shrink w-full bg-white">
+      <Ellipsis className="flex-initial shrink" />
+    </div>
+    <div className="flex-initial h-2 bg-gradient-to-b from-white" />
+  </div>
+);
+
+const Pointy = ({
+  pointTarget,
+  pointWidth,
+  pointOffset,
+  borderWidth,
+}: {
+  pointTarget: { x: number; y: number };
+  pointWidth: number;
+  pointOffset: number;
+  borderWidth: number;
+}) => {
+  const dims = {
+    w: Math.abs(pointTarget.x + borderWidth / 2),
+    h: Math.abs(pointTarget.y + borderWidth / 2),
+  };
+
+  // base css `right` distance
+  const rightBasis = borderWidth - Math.max(pointTarget.x - pointWidth, 0);
+  return (
+    <svg
+      version="1.1"
+      width={`${dims.w}`}
+      height={`${dims.h}`}
+      xmlns="http://www.w3.org/2000/svg"
+      className={`absolute fill-white stroke-black`}
+      style={{
+        top: '100%',
+        right: `calc(${rightBasis}px + ${pointOffset}rem)`,
+      }}
+    >
+      <polyline
+        points={`0, 0 ${pointTarget.x}, ${pointTarget.y} ${pointWidth}, 0`}
+        strokeLinejoin="round"
+        strokeLinecap="round"
+        strokeWidth={borderWidth}
+      />
+    </svg>
+  );
+};
+
+export type Props = {
+  text: string;
+  resetTick: number;
+  className?: string;
+  bubbleClassName?: string;
+};
+
+export default ({ text, resetTick, className, bubbleClassName }: Props) => {
+  // TODO?: somehow extract borderWidth from paper class border width in pixels
+  const borderWidth = 4; // tailwind border-4 == 4px
+
+  const pointOffset = 1;
+  const pointWidth = 40;
+  const pointTarget = {
+    x: 80,
+    y: 40,
+  };
+
+  return (
+    <div className={`flex flex-col items-center relative paper ${className}`}>
+      <OverflowIndicator className={`absolute top-1 right-1 left-1`} />
+      <p className="flex-1 w-full overflow-y-auto">{text}</p>
+      <Pointy
+        borderWidth={borderWidth}
+        pointOffset={pointOffset}
+        pointWidth={pointWidth}
+        pointTarget={pointTarget}
+      />
+    </div>
+  );
+};
