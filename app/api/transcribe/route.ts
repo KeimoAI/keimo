ec45dcd65f895bf66c34c284d0823e13f635c005
Sergio Segrera <sergioarturosegrera@gmail.com>
2023-02-28 23:08:40 -0500

    Connect client to handler

--- app/api/transcribe/route.ts
+++ app/api/transcribe/route.ts
@@ -1,15 +1,36 @@
-import { exec } from "child_process";
+import { exec } from 'child_process';
+import { promises as fs } from 'fs';
 
-export async function GET() {
-    return new Response(
-        await new Promise((resolve, reject) => {
-            exec(`sudo whisper /tmp/small-tesla.mp3 --fp16 False --model tiny --language English -f txt > /dev/null && cat small-tesla.mp3.txt`, (err, stdout, stderr) => {
-                if (err) {
-                    resolve(err.name);
-                } else {
-                    resolve(stdout);
-                }
-            })
-        })
+export async function POST(request: Request) {
+  const { sound } = await request.json();
+
+  if (!sound) {
+    return new Response('No sound provided', { status: 400 });
+  }
+
+  const base64Data = sound.replace(/^data:audio\/webm;base64,/, '');
+
+  const buffer = await Buffer.from(base64Data, 'base64');
+  await fs.writeFile('/tmp/sound.webm', buffer);
+
+  const result = await new Promise((resolve, reject) => {
+    exec(
+      `whisper /tmp/sound.webm --fp16 False --model tiny --language English`,
+      (err, stdout, stderr) => {
+        if (err) {
+          resolve(stderr);
+        } else {
+          resolve(stdout);
+        }
+      }
     );
+  });
+
+  console.log(result);
+
+  if (!result) {
+    return new Response('No transcription', { status: 400 });
+  }
+
+  return Response.json({ result });
 }
