aeff5afbc0dce6ed3f5a771b9a3f3ce367a831ed
Sergio Segrera <sergioarturosegrera@gmail.com>
2023-03-01 19:27:06 -0500

    Add Keimo to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,40 @@
+import Keimo from 'components/Keimo';
 import SpeakButton from 'components/SpeakButton';
 import SpeechBubble from 'components/SpeechBubble';
-import Image from 'next/image';
 import styles from './page.module.css';
 
 export default function Home() {
   return (
-    <main className={`${styles.main} bg-blue h-full flex flex-col min-w-0 p-4`}>
-      <div className="w-full flex-1 flex flex-col justify-end">
-        <SpeechBubble
-          className="w-96 mb-4"
-          point={{
-            offset: { left: 1 },
-            width: 50,
-            target: { x: -40, y: 40 },
-          }}
-          text={'test'}
-          resetTick={0}
-        />
-        <SpeechBubble
-          className="mr-[5rem] w-80 mb-14 self-end"
-          point={{
-            offset: { right: 1 },
-            width: 50,
-            target: { x: 80, y: 40 },
-          }}
-          bgColor="green"
-          text={'test'}
-          resetTick={0}
-        />
+    <main
+      className={`${styles.main} bg-blue h-full grid grid-cols-2 min-w-0 content-end`}
+    >
+      <Keimo className="self-end" width={600} height={600} />
+      <div className="flex self-end p-4">
+        <div className="w-full flex-1 flex flex-col justify-end pb-6">
+          <SpeechBubble
+            className="w-96 mb-4"
+            point={{
+              offset: { left: 1 },
+              width: 50,
+              target: { x: -40, y: 40 },
+            }}
+            text={'test'}
+            resetTick={0}
+          />
+          <SpeechBubble
+            className="mr-[5rem] w-80 mb-14 self-end"
+            point={{
+              offset: { right: 1 },
+              width: 50,
+              target: { x: 80, y: 40 },
+            }}
+            bgColor="green"
+            text={'test'}
+            resetTick={0}
+          />
+        </div>
+        <SpeakButton className="self-end" />
       </div>
-      <SpeakButton className="self-end" />
     </main>
   );
 }
