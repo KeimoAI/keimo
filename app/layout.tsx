7ca7ca488de337c5934ea35833066366b2409357
Javier A. Pollak <javi.po.123@gmail.com>
2023-03-01 22:34:29 -0500

    Make body bg blue
    This makes scaling the page *seem* smoother as there aren't any white
    fringes appearing.

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="h-full">
-      <body className="h-full">{children}</body>
+      <body className="h-full bg-blue">{children}</body>
     </html>
   );
 }
