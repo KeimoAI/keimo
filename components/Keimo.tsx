2bf4c60fdf34b47ee5d1cee7b76af5e1cd1782ec
Sergio Segrera <sergioarturosegrera@gmail.com>
2023-03-03 13:36:22 -0500

    Add listening, thinking animations

--- components/Keimo.tsx
+++ components/Keimo.tsx
@@ -6,8 +6,8 @@ import useKeimoStateStore, { State } from 'store/keimoStateStore';
 
 import idleAnimation from 'public/animations/keimo-idle.json';
 import keimoMouthAnimation from 'public/animations/keimo-mouth.json';
-// import listeningAnimation from 'public/animations/keimo-listening.json';
-// import thinkingAnimation from 'public/animations/keimo-thinking.json';
+import listeningAnimation from 'public/animations/keimo-listening.json';
+import thinkingAnimation from 'public/animations/keimo-thinking.json';
 // import speakingAnimation from 'public/animations/keimo-speaking.json';
 
 export default function Keimo({
@@ -18,64 +18,56 @@ export default function Keimo({
   className?: string;
 }) {
   const state = useKeimoStateStore((store) => store.state);
-  const [animation, setAnimation] = useState(idleAnimation);
-  const keimoRef = useRef(null);
-  const keimoMouthRef = useRef(null);
-
-  useEffect(() => {
-    if (!keimoRef.current) return;
-
-    switch (state) {
-      case State.IDLE:
-        setAnimation(idleAnimation);
-        break;
-
-      case State.LISTENING:
-        break;
-
-      case State.THINKING:
-        // setAnimation(thinkingAnimation);
-        break;
-
-      case State.SPEAKING:
-        // setAnimation(speakingAnimation);
-        break;
-    }
-  }, [state]);
 
   return (
-    <div className={className}>
+    <div
+      className={`${className} relative`}
+      style={{
+        width,
+        height: width,
+        marginTop: -width / 2,
+        marginLeft: `-${(80 * width) / 600}px`,
+        marginRight: `-${(140 * width) / 600}px`,
+        overflowY: 'hidden',
+      }}
+    >
       <Player
-        className={`relative`}
-        ref={keimoRef}
+        className={`absolute z-0 bottom-0`}
         autoplay
         loop
-        src={animation}
+        src={idleAnimation}
         style={{
-          width,
-          marginTop: -width / 2,
-          marginLeft: `-${(80 * width) / 600}px`,
-          marginRight: `-${(140 * width) / 600}px`,
-          overflowY: 'hidden',
+          display:
+            state === State.IDLE || state === State.SPEAKING ? 'block' : 'none',
         }}
-      >
-        <Player
-          className={`absolute z-10 bottom-0 invisible ${
-            state === State.SPEAKING && 'visible'
-          }}`}
-          ref={keimoMouthRef}
-          autoplay
-          loop
-          src={keimoMouthAnimation}
-          style={{
-            width,
-            marginTop: -width / 2,
-            marginLeft: `-${(80 * width) / 600}px`,
-            marginRight: `-${(140 * width) / 600}px`,
-            overflowY: 'hidden',
-          }}
-        />
-      </Player>
+      />
+      <Player
+        className={`absolute z-10 bottom-0`}
+        autoplay
+        loop
+        src={keimoMouthAnimation}
+        style={{
+          display: state === State.SPEAKING ? 'block' : 'none',
+        }}
+      />
+      <Player
+        className={`absolute z-10 bottom-0`}
+        autoplay
+        loop
+        src={listeningAnimation}
+        style={{
+          display: state === State.LISTENING ? 'block' : 'none',
+        }}
+      />
+      <Player
+        className={`absolute z-10 bottom-0`}
+        autoplay
+        loop
+        src={thinkingAnimation}
+        style={{
+          display: state === State.THINKING ? 'block' : 'none',
+        }}
+      />
     </div>
   );
 }
