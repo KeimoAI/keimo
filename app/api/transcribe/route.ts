4933ea62887545b2595062d037216ba588d1565e
Sergio Segrera <sergioarturosegrera@gmail.com>
2023-03-11 23:22:04 -0500

    Add next transcribe handler

--- app/api/transcribe/route.ts
+++ app/api/transcribe/route.ts
@@ -1,15 +1,69 @@
-import { exec } from "child_process";
+import { Configuration, OpenAIApi } from 'openai';
+import fs from 'fs/promises';
+import { createReadStream } from 'fs';
+import textToSpeech from '@google-cloud/text-to-speech';
 
-export async function GET() {
-    return new Response(
-        await new Promise((resolve, reject) => {
-            exec(`sudo whisper /tmp/small-tesla.mp3 --fp16 False --model tiny --language English -f txt > /dev/null && cat small-tesla.mp3.txt`, (err, stdout, stderr) => {
-                if (err) {
-                    resolve(err.name);
-                } else {
-                    resolve(stdout);
-                }
-            })
-        })
-    );
+const config = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const WHISPER_PROMPT =
+  'The transcript is about a child speaking to an anthropomorphic half-bear half-fox character called Keimo. The child asks questions about school subjects or interests they are curious about.';
+
+const GPT_PROMPT =
+  'You are an anthropomorphic half-bear half-fox character called Keimo. Your help children aged 6-12 with questions and comments. Respond to them in a fun, brief and concise manner. Use appropriate and easy to understand language for children. Use descriptive statements without bias. Generate very short responses. You are an interactive tutor that responds to questions and raises questions for the student to answer.';
+
+export async function POST(request: Request) {
+  const { sound } = await request.json();
+
+  if (!sound) {
+    return new Response('No sound provided', { status: 400 });
+  }
+
+  const base64Data = sound.replace(/^data:audio\/webm;base64,/, '');
+
+  const buffer = await Buffer.from(base64Data, 'base64');
+  await fs.writeFile('/tmp/sound.webm', buffer);
+
+  const openai = new OpenAIApi(config);
+
+  const { data } = await openai.createTranscription(
+    createReadStream('/tmp/sound.webm') as any,
+    'whisper-1',
+    WHISPER_PROMPT
+  );
+
+  const query = data.text;
+
+  const res = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    messages: [
+      { role: 'system', content: GPT_PROMPT },
+      { role: 'user', content: query },
+    ],
+  });
+
+  const answer = res.data.choices[0].message;
+  console.log(answer);
+
+  // TODO: Possible remove markdown before sending it to the TTS endpoint
+  const textToSpeechClient = new textToSpeech.TextToSpeechClient();
+
+  const [speechResponse] = await textToSpeechClient.synthesizeSpeech({
+    input: { text: answer?.content },
+    voice: { languageCode: 'en-US', ssmlGender: 'NEUTRAL' },
+    audioConfig: { audioEncoding: 'OGG_OPUS' },
+  });
+
+  console.log(speechResponse);
+  // Turn speechResponse into a base64 string
+  const base64 = speechResponse.audioContent?.toString('base64');
+
+  return Response.json({
+    query,
+    response: {
+      audio: base64,
+      text: answer?.content,
+    },
+  });
 }
