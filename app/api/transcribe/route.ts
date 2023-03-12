196305155ec3afc495705d8f5e5d60ed422a415d
Sergio Segrera <sergioarturosegrera@gmail.com>
2023-03-12 18:03:22 -0400

    Add tts env credentials

--- app/api/transcribe/route.ts
+++ app/api/transcribe/route.ts
@@ -4,16 +4,30 @@ import { createReadStream } from 'fs';
 import textToSpeech from '@google-cloud/text-to-speech';
 import { NextResponse as Response } from 'next/server';
 
+// OpenAI Configuration
 const config = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
+// Google Cloud Text-to-Speech Configuration
+const textToSpeechClient = new textToSpeech.TextToSpeechClient({
+  credentials: {
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    private_key: process.env.GOOGLE_PRIVATE_KEY,
+  },
+  //  email: process.env.GOOGLE_CLIENT_EMAIL,
+  projectId: process.env.GOOGLE_PROJECT_ID,
+});
+
+// Whisper Context Prompt
 const WHISPER_PROMPT =
   'The transcript is about a child speaking to an anthropomorphic half-bear half-fox character called Keimo. The child asks questions about school subjects or interests they are curious about.';
 
+// Starting ChatGPT Prompt
 const GPT_PROMPT =
   'You are an anthropomorphic half-bear half-fox character called Keimo. Your help children aged 6-12 with questions and comments. Respond to them in a fun, brief and concise manner. Use appropriate and easy to understand language for children. Use descriptive statements without bias. Generate very short responses. You are an interactive tutor that responds to questions and raises questions for the student to answer.';
 
+// TODO: Add better error handling
 export async function POST(request: Request) {
   const { sound } = await request.json();
 
@@ -48,11 +62,11 @@ export async function POST(request: Request) {
   console.log(answer);
 
   // TODO: Possible remove markdown before sending it to the TTS endpoint
-  const textToSpeechClient = new textToSpeech.TextToSpeechClient();
+  // TODO: Use SSML to add pauses and other things...
 
   const [speechResponse] = await textToSpeechClient.synthesizeSpeech({
     input: { text: answer?.content },
-    voice: { languageCode: 'en-US', ssmlGender: 'NEUTRAL' },
+    voice: { languageCode: 'en-US', ssmlGender: 'FEMALE' },
     audioConfig: { audioEncoding: 'OGG_OPUS' },
   });
 
